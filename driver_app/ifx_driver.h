7b340260888ba06ad91643dd164c9b4f8e191bc0
cjkring <cjkring@gmail.com>
2021-01-08 10:54:11 -0800

    CCommit before moving to Mac

---
+++ driver_app/ifx_driver.h
@@ -0,0 +1,8 @@
+#ifdef IFX_DRIVER_H
+#define IXF_DRIVER_H
+
+#define ifx_driver_VERSION_MAJOR @ixf_driver_VERSION_MAJOR
+#define ifx_driver_VERSION_MINOR @ixf_driver_VERSION_MINOR
+
+
+#endif // IXF_DRIVER_H
