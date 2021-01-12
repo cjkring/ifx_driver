85d951e300647e7eb92a7c1d980ba80da16fc59c
cjkring <cjkring@gmail.com>
2021-01-11 16:32:41 -0800

    More merges for Mac build

---
+++ driver_app/ifx_app.c
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define WIN32
+#ifdef WIN32
+//#define _WINNT_WIN32 0x601
+#include <winsock2.h>
+#include <stdint.h>
+//#define _WIN32_WINNT 0x501
+#include <ws2tcpip.h>
+typedef int socklen_t;
+#else // Linux
+#define closesocket close
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h> 
+#endif
+#include <stdlib.h> 
+#include <string.h> 
+#include <signal.h>
+
+//#pragma comment(lib, "ws2_32.lib") // winsock library
+void ClearWinSock() {
+#if defined WIN32
+    WSACleanup();
+#endif
+}
+
+static char *port = "8080";
+static int server_fd = -1;
+static struct sockaddr_in address; 
+
+int setupServerSocket()
+{
+    int opt = 0; 
+
+#ifdef WIN32
+
+    WSADATA wsaData;
+    if( WSAStartup(MAKEWORD(2,2), &wsaData) < 0 ){
+        fprintf(stderr, "Error: WSAstartup failed\n");
+        return -1;
+    }
+
+    struct addrinfo hints = {0};
+    struct addrinfo *res = NULL;
+    
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    if( getaddrinfo(NULL, "8080", &hints, &res ) < 0  ){
+        fprintf(stderr, "getaddrinfo failed\n");
+        return -1;
+    }
+
+    // Creating socket file descriptor 
+    if ((server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0 ) { 
+        fprintf(stderr, "socket failed: %d\n", WSAGetLastError()); 
+        return -1;
+    } 
+       
+#else 
+    signal(SIGPIPE, SIG_IGN);
+
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) { 
+        perror("setsockopt failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+#endif
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, res->ai_addr,  res->ai_addrlen) < 0) { 
+        fprintf(stderr, "bind failed: %d\n", WSAGetLastError()); 
+        return -1;
+    } 
+    if (listen(server_fd, 3) < 0) { 
+        fprintf(stderr, "listen failed %d\n", WSAGetLastError()); 
+        return -1;
+    } 
+    return server_fd;
+}
+int acceptSocket()
+{
+    socklen_t addrlen = sizeof(address);
+    int sock;
+    if ((sock = accept(server_fd, (struct sockaddr *)&address,  &addrlen)) < 0) { 
+        fprintf(stderr, "accept failed %d\n", WSAGetLastError()); 
+        return -1;
+    } 
+    return sock;
+}
+typedef struct reading {
+    uint32_t I;
+    uint32_t Q;
+} reading;
+#define SAMPLES_PER_PACKET  256
+
+int notmain(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+
+    reading readings[SAMPLES_PER_PACKET];
+  
+
+    double scale = 2;
+
+    if( argc > 1 ){
+        scale = atof(argv[1]);
+        fprintf(stderr, "scale set to %f\n", scale);
+    }
+    server_fd = setupServerSocket();
+    // handles only one socket at a time.  Dumps I Q to the socket
+#define SECOND_NANOS 1000000000L
+#define PADDING_LEN 4
+    double update_period = SECOND_NANOS / scale;
+    uint8_t padding[PADDING_LEN] = {0xDE,0xAD,0xBE,0xEF};
+    
+    uint32_t sin_count = 0;
+    while( 1 ){
+        new_socket = acceptSocket();
+        uint32_t seqno = 1;
+        while( 1 ){   // socket is connected
+
+        float I, Q;
+
+            for( int count = 0; count < SAMPLES_PER_PACKET; count++ ){
+		// base rate is 512 count per sec / 6.28 count per radian = 81 Hz
+		double theta = sin_count / 0.5;
+                I = sin(theta); // 41 Hz
+                Q = cos(2 * theta);
+                fprintf(stderr,"%d,%2.2f,%2.2f\n", sin_count, I, Q );
+                
+
+                readings[count].I = htonl(I*1000 + 100000);
+                readings[count].Q = htonl(Q*1000 + 100000);
+                if( ++sin_count > 10000000 )sin_count = 0;
+            }
+        int count_hton = htonl(SAMPLES_PER_PACKET);
+        int seq_hton = htonl(seqno);
+        if(( send(new_socket, padding, PADDING_LEN, 0) < 0 ) ||
+           ( send(new_socket, (char *)&seq_hton, sizeof(int32_t), 0) < 0 ) ||
+           ( send(new_socket, (char *)&count_hton, sizeof(int32_t), 0) < 0 ) ||
+           ( send(new_socket, (char *)readings, sizeof(int32_t) * 2 * SAMPLES_PER_PACKET, 0) < 0 )){
+                fprintf(stderr, "Socket disconnected");
+                break;
+            }
+            seqno++;
+            //nanosleep((const struct timespec[]){{0, update_period }}, NULL);
+            nanosleep((const struct timespec[]){{1, 0}}, NULL);
+        }
+    }
+    return 0; 
+} 
